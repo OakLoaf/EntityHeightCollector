69a03bae1c579f70dd3ef4d926d55b276076927e
Oak <cooldaveb@live.co.uk>
2024-07-24 16:31:18 +0100

    Correct java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 
     registerFeature("optional") {
         usingSourceSet(sourceSets["main"])
