d29e45ab0c3f81f1c71d0815edbaecdecfbecc7b
Oak <cooldaveb@live.co.uk>
2024-07-24 16:19:21 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    `java-library`
+    id("io.github.goooler.shadow") version("8.1.7")
+}
+
+
+group = "org.lushplugins"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven("https://oss.sonatype.org/content/groups/public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") // Spigot
+}
+
+dependencies {
+    // Dependencies
+    compileOnly("org.spigotmc:spigot-api:1.21-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+
+    registerFeature("optional") {
+        usingSourceSet(sourceSets["main"])
+    }
+
+    withSourcesJar()
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+    }
+
+    shadowJar {
+        minimize()
+
+        val folder = System.getenv("pluginFolder_1-21")
+        if (folder != null) {
+            destinationDirectory.set(file(folder))
+        }
+
+        archiveFileName.set("${project.name}-${project.version}.jar")
+    }
+
+    processResources{
+        expand(project.properties)
+
+        inputs.property("version", rootProject.version)
+        filesMatching("plugin.yml") {
+            expand("version" to rootProject.version)
+        }
+    }
+}
